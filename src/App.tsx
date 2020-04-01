825e3d013c34ce921fab8e53d44eb4ad4a022403
wchestnuti27 <wchestnuti27@users.noreply.github.com>
2020-04-01 15:21:36 -0400

    updated React app to Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App: React.FunctionComponent = () => {
   return (
-    <div>
+    <div className="App">
+
     </div>
   );
 }
