4c2c5be36099befbfdf11f66ed378994480b7b7b
wchestnuti27 <wchestnuti27@users.noreply.github.com>
2020-04-01 14:42:10 -0400

    starting scaffolding out component folders

---
+++ src/components/CRUD/Memes/Memes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Memes extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
