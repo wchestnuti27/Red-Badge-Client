51dde90fc4855495d0e482beec13a5d86745712e
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-04-02 15:28:26 -0400

    tested meme retrieval from db

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Feed from './components/CRUD/Feed/Feed';
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-
+      <Feed />
     </div>
   );
 }
