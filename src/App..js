cdcc0b1955404e65d5a0cabeb5bd20f6c6e2670f
wchestnuti27 <wchestnuti27@users.noreply.github.com>
2020-04-06 10:07:17 -0400

    updated Signin component, added node.sass dependency, added index.jsx in component as well

---
+++ src/App..js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.scss';
+// import Feed from './components/CRUD/Feed/Feed';
+import { Login, Signup } from "./components/Signin/index";
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogginActive: true,
+    };
+  }
+
+  componentDidMount () {
+    // add .right by default //
+    this.rightSide.classList.add('right');
+  }
+
+  changeState() {
+    const { isLogginActive } = this.state;
+
+    if(isLogginActive) {
+      this.rightSide.classList.remove('right');
+      this.rightSide.classList.add('left');
+    } else {
+      this.rightSide.classList.remove('left');
+      this.rightSide.classList.add('right');
+    }
+
+    this.setState((prevState) => ({ isLogginActive: !prevState.isLogginActive }));
+  }
+
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Signup" : "Login";
+    const currentActive = isLogginActive ? "Login" : "Signup";
+    return (
+      <div className="App">
+        <div className="login">
+          <div className="container" ref={ref => (this.container = ref)}>
+            {isLogginActive && (
+              <Login containerRef={ref => (this.current = ref)} />
+            )}
+            {!isLogginActive && (
+              <Signup containerRef={ref => (this.current = ref)} />
+            )}
+          </div>
+          <RightSide
+            current={current}
+            currentActive={currentActive}
+            containerRef={ref => (this.rightSide = ref)}
+            onClick={this.changeState.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+const RightSide = props => {
+  return <div className= "right-side" ref={props.containerRef} onClick={props.onClick}>
+    <div className='inner-container'>
+    <div className="text">{props.current}></div>
+  </div>
+  </div>
+
+}
+
+export default App;
