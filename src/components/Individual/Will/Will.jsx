49a506cd99888b4ec7cdf2ee7aae40aa01c7ce9e
wchestnuti27 <wchestnuti27@users.noreply.github.com>
2020-04-13 11:20:10 -0400

    adding slash to fix discrepancy with push/pull request

--- src/components/Individual/Will/Will.jsx
+++ src/components/Individual/Will/Will.jsx
@@ -16,20 +16,6 @@ class Will extends React.Component {
 
     componentDidMount() {
         console.log("record found");
-<<<<<<< HEAD
-    
-    fetch(`https://cors-anywhere.herokuapp.com/http://www.boredapi.com/api/activity/`)
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        this.setState({
-            activity: json.activity,
-            type: json.type,
-        })
-        console.log(this.state.activity);
-        console.log(this.state.type);
-    });
-=======
 
         fetch(`https://www.boredapi.com/api/activity/`)
             .then(response => response.json())
@@ -42,7 +28,6 @@ class Will extends React.Component {
                 console.log(this.state.activity);
                 console.log(this.state.type);
             });
->>>>>>> 03b114ddd59cae10d00b644eb7875dc6eb71ed14
     }
     // }
 
